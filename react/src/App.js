f5ad10bd90cbf7ebd6b2a76e72c1681d4c95875a
sarahfcader <sarah.f.cader@gmail.com>
2022-08-02 00:14:49 -0700

    Basic frontend

--- react/src/App.js
+++ react/src/App.js
@@ -6,24 +6,33 @@ export default function App() {
   const wave = () => {
 
   }
-  
+
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mainContainer">
+
+      <div className="dataContainer">
+        <div className="header">
+          👋 Hello!
+        </div>
+        
+        <div className="bio">
+          I'm Sarah, a computer science undergrad at UBC and growth manager at KLAP Finance. I'm passionate about DeFi and the future of Web3!
+        </div>
+
+        <button className="waveButton" onClick={wave}>
+          Wave at Me
+        </button>
+
+        <button className="connectWalletButton">
+          Connect Wallet
+        </button>
+
+        <div className="footer"> 
+          Sarah Cader © 2022
+        </div>
+      </div>
+
     </div>
   );
 }
